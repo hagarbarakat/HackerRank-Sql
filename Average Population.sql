e534b806a0600b7c30129f09a1ab7841a2950861
Hagar Barakat <hagarbarakat@hotmail.com>
2020-07-03 17:19:22 +0200

    Update Average Population.sql

--- Average Population.sql
+++ Average Population.sql
@@ -1,3 +1,3 @@
 select round(avg(population),0.5) from city;
-------------------------------------------------------
+/*------------------------------------------------------*/
 select floor(avg(population)) from city;
